0db38ed64ff6c06d8ae9c4f3d1f969bfc0ea828e
Luke <luke.wangai@student.moringaschool.com>
2025-03-11 15:46:51 +0300

    Function Out!

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+
+return `This Saturday, I want to ${activity}!`
+   
+    
+}
+
+// console.log(saturdayFun());
+
+
+
+
+    
+function mondayWork(activity = "go to the office") {
+return `This Monday, I will ${activity}.`
+}
+
+
+
+function wrapAdjective(adj= "*") {
+    return function inner(good = "special"){
+        return `You are ${adj}${good}${adj}!`
+ 
+        }
+        
+    
+    }
+
+
+
+
+
+
